7f0d539868db559fdb530b6f069a5f988ecf0b7d
yabuki829 <syodai829@icloud.com>
2024-03-16 21:11:32 +0900

    コマンドを追加

---
+++ api/management/commands/initial_command.py
@@ -0,0 +1,9 @@
+from django.core.management.base import BaseCommand
+from utils.broadcaster import Broadcaster
+
+class Command(BaseCommand):
+    help = 'Prints all book titles in the database'
+    def handle(self, *args, **options):
+        broadcaster = Broadcaster()
+        broadcaster.add_broadcast
+        
--- api/management/commands/mycommand.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-
-
-class Command(BaseCommand):
-    help = "テストコマンド"
-
-    def handle(self, *args, **options):
-        print('Hello World!')
